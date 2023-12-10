8de25eb5d2e9a597d396af8626e5988db4f8d6cf
Natnael Demelash <natidemelash074@gmail.com>
2023-12-10 16:49:07 +0300

    fetch game genres

---
+++ src/components/GenreLists.tsx
@@ -0,0 +1,15 @@
+import useGenre from "../hooks/useGenre";
+
+const GenreLists = () => {
+  const { genres } = useGenre();
+
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}> {genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreLists;
